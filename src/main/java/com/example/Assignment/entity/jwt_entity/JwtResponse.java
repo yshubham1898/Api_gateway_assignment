53f7f633a89d6bf1737438ad3f990f074fe3cacc
yshubham1898 <yshubham1898@gmail.com>
2022-06-24 01:35:26 +0530

    adding jwt dependencies

---
+++ src/main/java/com/example/Assignment/entity/jwt_entity/JwtResponse.java
@@ -0,0 +1,12 @@
+package com.example.Assignment.entity.jwt_entity;
+
+public class JwtResponse {
+
+    private String jwt;
+
+    public JwtResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+
+}
